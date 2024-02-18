bcdf0724102c23afd75e958272c8445bd360ef00
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-02-18 17:17:30 +0100

    implementata registrazione

--- 15_02_24/resources/views/auth/login.blade.php
+++ 15_02_24/resources/views/auth/login.blade.php
@@ -1,17 +1,15 @@
 <x-main>
     <form action="/login" method="POST" class="mt-5 mx-auto col-lg-6">
+    @csrf
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="email" class="form-control" id="email" aria-describedby="email" name="email">
+        @error('email') <span class="small text-danger">{{$message}}</span>@enderror
     </div>
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <input type="password" class="form-control" id="password" name="password">
+        @error('password') <span class="small text-danger">{{$message}}</span>@enderror
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     </form>
